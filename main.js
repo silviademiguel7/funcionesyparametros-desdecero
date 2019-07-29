2b8f726ff5530ccc4622c8e7f18ae7ed603cdbb5
silvia <silviademiguel7@gmail.com>
2019-07-29 17:44:15 +0200

    fase 2 correct

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 const patataString = 'Patata';
 
 function ten(texto){
-console.log(texto);
+    for (let i = 0; i < 10; i++){
+        console.log(texto);
+    }
 }
+
 const words = ['Patata','Aguacate','Pizza'];
-for (let i = 0; i < words.length; i++){
-    ten(words[i]);
+for(let j= 0; j < words.length ; j++){
+    ten(words[j]);
 }
