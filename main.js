fddcaeb73288cff54352d222c496000cb5d150a3
silvia <silviademiguel7@gmail.com>
2019-07-29 18:04:20 +0200

    fase 4 done

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ function writeThis(texto,repeat){
 
 const words = ['Patata','Aguacate','Pizza'];
 const repeat = [10,7,50];
+const max=10;
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
 
 for(let j= 0; j < words.length ; j++){
-    writeThis(words[j],repeat[j]);
+    writeThis(words[j],getRandomNumber(max));
 }
