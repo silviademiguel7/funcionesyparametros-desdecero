985270abfd9f1926d314eb8d2b8a9dcc1d35e3fd
silvia <silviademiguel7@gmail.com>
2019-07-29 17:58:22 +0200

    fase3 done

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 'use strict';
 const patataString = 'Patata';
 
-function ten(texto){
-    for (let i = 0; i < 10; i++){
+function writeThis(texto,repeat){
+    for (let i = 0; i < repeat; i++){
         console.log(texto);
     }
 }
 
 const words = ['Patata','Aguacate','Pizza'];
+const repeat = [10,7,50];
+
 for(let j= 0; j < words.length ; j++){
-    ten(words[j]);
+    writeThis(words[j],repeat[j]);
 }
